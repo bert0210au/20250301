17ca08a907dabc5e17a88c9685f1fe4a4f012ccf
Bert Chen <bert0210au@gmail.com>
2025-03-15 12:37:54 +0800

    no message

---
+++ 03_exp_guess/src/main/java/com/course/controller/GuessController.java
@@ -0,0 +1,39 @@
+package com.course.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.course.service.GuessService;
+
+@Controller
+public class GuessController {
+	
+	@Autowired
+	private GuessService guessService;
+	
+	@GetMapping("/")
+	public String home() {
+		return "index";
+	}
+	
+	@GetMapping("/newGame")
+	public String newGame() {
+		System.out.println("newGame");
+		guessService.getAnswer();
+		return "index";
+	}
+	
+	@GetMapping("/guess")
+	public String guessAnswer(@RequestParam("guessNum") String guessNumber) {
+		System.out.println("guess: "+guessNumber);
+		return "index";
+	}
+	
+	@GetMapping("/testSession")
+	public String testSession() {
+		guessService.testSession();
+		return "index";
+	}
+}
---
+++ 03_exp_guess/src/main/webapp/WEB-INF/views/index.jsp
@@ -0,0 +1,19 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+
+<h1>1A2B 猜數字！</h1>
+
+<form action="guess" method="GET">
+	猜數字：<input type="text" name="guessNum" />
+	<button type="submit">猜答案</button>
+</form>
+
+</body>
+</html>
